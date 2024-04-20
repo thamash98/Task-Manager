06601679e1967770769d9811b12c5b8e321b8740
Thamash Karunarathna <thasaranga123@gmail.com>
2024-04-20 11:53:45 +0530

    add Task Model

---
+++ Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
